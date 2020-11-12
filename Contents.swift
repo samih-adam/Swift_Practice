ddcbaebcd87c5ea89ffc65b1f9752c2438197c89
Samieh Jarabba <abdels6375@gmail.com>
2020-11-12 01:01:05 -0600

    comment_practice

--- Contents.swift
+++ Contents.swift
@@ -1,6 +1,16 @@
 import UIKit
 
 var str = "Hello, playground"
+// this is a comment
 
+// practicing print statements
 
-print("Hello World")
+// print("Hello World")
+// if you hold down the cmd key as well as the forward / than it will automatically add the comment line
+
+/* This is a
+ Multi
+ Line
+ comment
+ for your code*/
+
