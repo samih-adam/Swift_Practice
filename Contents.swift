9d86b9881c0bec8b5ee3add3cc437a8f356cfcd7
Samieh Jarabba <abdels6375@gmail.com>
2020-11-12 01:37:44 -0600

    practicing_variables

--- Contents.swift
+++ Contents.swift
@@ -22,3 +22,17 @@ print("Hello \(2+3) World!")
 print("I am \(15+7) years old")
 
 print("The result of 5 + 3 = \(5 + 3) ")
+
+
+// Now we are going to look at Variables
+// a variable is set up like this --> var label =  data
+// so example is                      var Angela = 3147278888
+//connecting the variable to this persons phone #
+
+var a = 5
+var b = 8
+print (a)
+print(b)
+print(a + b)
+print("The value of a is \(a)")
+print("The value of b is \(b)")
